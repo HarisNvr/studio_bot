dc834b13320ce67abe3d7ff96893812bee1f7e5b
Haro Mikhaylov <www.haris@bk.ru>
2024-08-04 08:58:05 +0300

    Final structure, docker support and bug fixes!

--- bot_backend/bot_funcs/user_funcs.py
+++ bot_backend/bot_funcs/user_funcs.py
@@ -1,19 +1,118 @@
+from datetime import datetime
 from os import getenv
+from pathlib import Path
+from random import choice
 from time import sleep
 
 from dotenv import load_dotenv
-from sqlalchemy import delete, select
+from sqlalchemy import delete, select, update
+from sqlalchemy.exc import NoResultFound
 from sqlalchemy.orm import Session
 from telebot import types
 from telebot.apihelper import ApiTelegramException
 
-from bot_parts.constants import BOT, DEL_TIME
+from bot_parts.constants import BOT, DEL_TIME, ORG_NAME, ADMIN_IDS
+from bot_parts.dicts import get_lang_greet_text
 from sql_orm import (
-    get_user_db_id, record_message_id_to_db, Message, engine
+    get_user_db_id, record_message_id_to_db, Message, engine, User
 )
 
-
 load_dotenv()
+
+
+def start_help(message, keep_last_msg: bool = False):
+    user_first_name = message.chat.first_name
+    chat_id = message.chat.id
+    username = message.chat.username
+
+    markup = types.InlineKeyboardMarkup()
+    btn_soc_profiles = types.InlineKeyboardButton(
+        text='#МыВСети \U0001F4F1',
+        callback_data='soc_profiles'
+    )
+    btn_shop = types.InlineKeyboardButton(
+        text='Наш магазин  \U0001F6CD',
+        callback_data='shop'
+    )
+    btn_studio = types.InlineKeyboardButton(
+        text='О студии \U0001F393',
+        callback_data='studio'
+    )
+    btn_offsite = types.InlineKeyboardButton(
+        text='Выездные МК  \U0001F30D',
+        callback_data='offsite_workshops'
+    )
+    btn_tarot = types.InlineKeyboardButton(
+        text='Карты ТАРО \U00002728',
+        callback_data='tarot'
+    )
+    btn_clean = types.InlineKeyboardButton(
+        text=f'Очистить чат \U0001F9F9',
+        callback_data='clean'
+    )
+    markup.row(btn_studio, btn_shop)
+    markup.row(btn_offsite, btn_soc_profiles)
+    markup.row(btn_tarot, btn_clean)
+
+    if chat_id in ADMIN_IDS:
+        btn_admin = types.InlineKeyboardButton(
+            '\U0001F60E Кнопка администратора \U0001F60E',
+            callback_data='admin'
+        )
+        markup.row(btn_admin)
+
+    with Session(engine) as session:
+        try:
+            user_record = session.execute(
+                select(User).where(User.chat_id == chat_id)).scalar_one()
+
+            if (user_record.username != username or
+                    user_record.user_first_name != user_first_name):
+                session.execute(
+                    update(User).where(User.chat_id == chat_id).values(
+                        username=username,
+                        user_first_name=user_first_name
+                    )
+                )
+        except NoResultFound:
+            user_record = User(
+                chat_id=chat_id,
+                username=username,
+                user_first_name=user_first_name
+            )
+            session.add(user_record)
+            session.commit()
+
+        user_db_id = user_record.id
+
+    record_message_id_to_db(user_record.id, message.message_id)
+
+    if message.text == '/start':
+        sent_message = BOT.send_message(
+            chat_id,
+            f'<b>Здравствуйте, <u>{user_first_name}</u>! \U0001F642'
+            f'\nМеня зовут {BOT.get_me().username}.</b>'
+            f'\nЧем я могу вам помочь?',
+            parse_mode='html',
+            reply_markup=markup
+        )
+
+        record_message_id_to_db(user_record.id, sent_message.message_id)
+    else:
+        if not keep_last_msg:
+            BOT.delete_message(chat_id, message.id)
+        else:
+            pass
+        sleep(DEL_TIME)
+
+        sent_message = BOT.send_message(
+            chat_id,
+            get_lang_greet_text(user_first_name),
+            parse_mode='html',
+            reply_markup=markup
+        )
+
+        record_message_id_to_db(user_db_id, sent_message.message_id)
 
 
 def clean(message):
@@ -149,3 +248,105 @@ def soc_profiles(message):
     )
 
     record_message_id_to_db(user_db_id, sent_message.message_id)
+
+
+def tarot_start(message):
+    chat_id = message.chat.id
+    user_first_name = message.chat.first_name
+
+    with Session(engine) as session:
+        stmt = select(User).where(User.chat_id == chat_id)
+        user = session.execute(stmt).scalar()
+    last_tarot_date = user.last_tarot_date
+
+    today = datetime.today().date().strftime('%d-%m-%Y')
+
+    if chat_id in ADMIN_IDS:
+        BOT.delete_message(chat_id, message.id)
+        sleep(DEL_TIME)
+        tarot_main(message)
+        start_help(message, True)
+    else:
+        if last_tarot_date == today:
+            sent_message = BOT.send_message(
+                message.chat.id,
+                f'<u>{user_first_name}</u>, '
+                f'вы уже сегодня получили расклад, попробуйте завтра!',
+                parse_mode='html'
+            )
+
+            record_message_id_to_db(user.id, sent_message.message_id)
+        else:
+            with Session(engine) as session:
+                session.execute(
+                    update(User).where(
+                        User.chat_id == chat_id
+                    ).values(
+                        last_tarot_date=today
+                    )
+                )
+                session.commit()
+
+            BOT.delete_message(chat_id, message.id)
+            sleep(DEL_TIME)
+
+            tarot_main(message)
+
+            start_help(message, True)
+
+
+def tarot_main(message):
+    chat_id = message.chat.id
+    user_db_id = get_user_db_id(chat_id)
+    tarot_delay = 1.5
+
+    tarot_path = Path(__file__).parent / '..' / 'Tarot'
+
+    sent_message = BOT.send_message(
+        message.chat.id,
+        '<b>Расклад Таро - это всего лишь инструмент для '
+        'ознакомления и развлечения. '
+        'Расклад карт Таро не является истиной и не должен '
+        'использоваться для принятия важных решений.</b>'
+        '\n'
+        f'\n<u>{ORG_NAME}</u> и его сотрудники не несут '
+        'ответственности за любые действия и их последствия, '
+        'которые повлекло использование данного расклада карт Таро.',
+        parse_mode='html'
+    )
+    sleep(tarot_delay)
+
+    record_message_id_to_db(user_db_id, sent_message.message_id)
+
+    cards = list(tarot_path.glob('*.jpg'))
+    user_random_cards = []
+
+    while len(user_random_cards) < 3:
+        card = choice(cards)
+        card_num = int(card.stem)
+
+        if card_num not in [int(c.stem) for c in user_random_cards]:
+            if (card_num % 2 == 1 and card_num + 1 not in
+                    [int(c.stem) for c in user_random_cards]):
+                user_random_cards.append(card)
+            elif (card_num % 2 == 0 and card_num - 1 not in
+                  [int(c.stem) for c in user_random_cards]):
+                user_random_cards.append(card)
+
+    captions = ['Прошлое', 'Настоящее', 'Будущее']
+
+    for card, caption in zip(user_random_cards, captions):
+        with card.open('rb') as photo:
+            text_file = card.with_suffix('.txt')
+            with text_file.open(encoding='utf-8') as text:
+                description = text.read()
+
+            tarot_message = BOT.send_photo(
+                message.chat.id, photo,
+                caption=f'<b>{caption}</b>: {description}',
+                parse_mode='html'
+            )
+
+            record_message_id_to_db(user_db_id, tarot_message.message_id)
+
+            sleep(tarot_delay)
--- bot_backend/bot_main.py
+++ bot_backend/bot_main.py
@@ -1,32 +1,20 @@
-from datetime import datetime
-from glob import glob
-from os import getenv, path, getcwd
-from random import choice
-from sys import platform
-from time import sleep
+from os import getenv
 
 from dotenv import load_dotenv
-from sqlalchemy import select, update
-from sqlalchemy.exc import NoResultFound
+from sqlalchemy import select
 from sqlalchemy.orm import Session
-from telebot import types
 
-from bot_funcs.admin_only import (
-    admin, proportions, send_user_count
-)
-from bot_funcs.shop_delivery import (
-    shop, catalog, ordering, payment, shipment
-)
-from bot_funcs.user_funcs import (
-    clean, delete_user_messages, soc_profiles
-)
+from bot_funcs.admin_only import admin, proportions, send_user_count
 from bot_funcs.broadcast import start_broadcast
-from bot_parts.constants import ADMIN_IDS, BOT, DEL_TIME, ORG_NAME
-from bot_parts.dicts import get_lang_greet_text
+from bot_funcs.shop_delivery import shop, catalog, ordering, payment, shipment
 from bot_funcs.studio_and_directions import (
     candles_info, custom_cloth, gips_info, epoxy,
     sketching, tie_dye_info, directions, studio, offsite_workshops
 )
+from bot_funcs.user_funcs import (
+    clean, delete_user_messages, soc_profiles, start_help, tarot_start
+)
+from bot_parts.constants import ADMIN_IDS, BOT
 from sql_orm import (
     engine, record_message_id_to_db, User, get_user_db_id, morning_routine
 )
@@ -82,102 +70,6 @@ def check_is_admin(func):
     return wrapper
 
 
-@BOT.message_handler(commands=['start', 'help'])
-def start_help(message, keep_last_msg: bool = False):
-    user_first_name = message.chat.first_name
-    chat_id = message.chat.id
-    username = message.chat.username
-
-    markup = types.InlineKeyboardMarkup()
-    btn_soc_profiles = types.InlineKeyboardButton(
-        text='#МыВСети \U0001F4F1',
-        callback_data='soc_profiles'
-    )
-    btn_shop = types.InlineKeyboardButton(
-        text='Наш магазин  \U0001F6CD',
-        callback_data='shop'
-    )
-    btn_studio = types.InlineKeyboardButton(
-        text='О студии \U0001F393',
-        callback_data='studio'
-    )
-    btn_offsite = types.InlineKeyboardButton(
-        text='Выездные МК  \U0001F30D',
-        callback_data='offsite_workshops'
-    )
-    btn_tarot = types.InlineKeyboardButton(
-        text='Карты ТАРО \U00002728',
-        callback_data='tarot'
-    )
-    btn_clean = types.InlineKeyboardButton(
-        text=f'Очистить чат \U0001F9F9',
-        callback_data='clean'
-    )
-    markup.row(btn_studio, btn_shop)
-    markup.row(btn_offsite, btn_soc_profiles)
-    markup.row(btn_tarot, btn_clean)
-
-    if chat_id in ADMIN_IDS:
-        btn_admin = types.InlineKeyboardButton(
-            '\U0001F60E Кнопка администратора \U0001F60E',
-            callback_data='admin'
-        )
-        markup.row(btn_admin)
-
-    with Session(engine) as session:
-        try:
-            user_record = session.execute(
-                select(User).where(User.chat_id == chat_id)).scalar_one()
-
-            if (user_record.username != username or
-                    user_record.user_first_name != user_first_name):
-                session.execute(
-                    update(User).where(User.chat_id == chat_id).values(
-                        username=username,
-                        user_first_name=user_first_name
-                    )
-                )
-        except NoResultFound:
-            user_record = User(
-                chat_id=chat_id,
-                username=username,
-                user_first_name=user_first_name
-            )
-            session.add(user_record)
-            session.commit()
-
-        user_db_id = user_record.id
-
-    record_message_id_to_db(user_record.id, message.message_id)
-
-    if message.text == '/start':
-        sent_message = BOT.send_message(
-            chat_id,
-            f'<b>Здравствуйте, <u>{user_first_name}</u>! \U0001F642'
-            f'\nМеня зовут {BOT.get_me().username}.</b>'
-            f'\nЧем я могу вам помочь?',
-            parse_mode='html',
-            reply_markup=markup
-        )
-
-        record_message_id_to_db(user_record.id, sent_message.message_id)
-    else:
-        if not keep_last_msg:
-            BOT.delete_message(chat_id, message.id)
-        else:
-            pass
-        sleep(DEL_TIME)
-
-        sent_message = BOT.send_message(
-            chat_id,
-            get_lang_greet_text(user_first_name),
-            parse_mode='html',
-            reply_markup=markup
-        )
-
-        record_message_id_to_db(user_db_id, sent_message.message_id)
-
-
 @BOT.message_handler(commands=['proportions', 'users', 'broadcast'])
 @check_bd_chat_id
 @check_is_admin
@@ -190,11 +82,13 @@ def admin_commands(message):
         start_broadcast(message)
 
 
-@BOT.message_handler(commands=['clean', 'studio', 'mk', 'shop',
-                               'soc_profiles'])
+@BOT.message_handler(commands=['start', 'help', 'clean', 'studio', 'mk',
+                               'shop', 'soc_profiles'])
 @check_bd_chat_id
 def user_commands(message):
-    if message.text == '/clean':
+    if message.text == ('/start' or '/help'):
+        start_help(message)
+    elif message.text == '/clean':
         clean(message)
     elif message.text == '/studio':
         studio(message)
@@ -204,118 +98,6 @@ def user_commands(message):
         shop(message)
     elif message.text == '/soc_profiles':
         soc_profiles(message)
-
-
-def tarot_start(message):
-    chat_id = message.chat.id
-    user_first_name = message.chat.first_name
-
-    with Session(engine) as session:
-        stmt = select(User).where(User.chat_id == chat_id)
-        user = session.execute(stmt).scalar()
-    last_tarot_date = user.last_tarot_date
-
-    today = datetime.today().date().strftime('%d-%m-%Y')
-
-    if chat_id in ADMIN_IDS:
-        BOT.delete_message(chat_id, message.id)
-        sleep(DEL_TIME)
-        tarot_main(message)
-        start_help(message, True)
-    else:
-        if last_tarot_date == today:
-            sent_message = BOT.send_message(
-                message.chat.id,
-                f'<u>{user_first_name}</u>, '
-                f'вы уже сегодня получили расклад, попробуйте завтра!',
-                parse_mode='html'
-            )
-
-            record_message_id_to_db(user.id, sent_message.message_id)
-        else:
-            with Session(engine) as session:
-                session.execute(
-                    update(User).where(
-                        User.chat_id == chat_id
-                    ).values(
-                        last_tarot_date=today
-                    )
-                )
-                session.commit()
-
-            BOT.delete_message(chat_id, message.id)
-            sleep(DEL_TIME)
-
-            tarot_main(message)
-
-            start_help(message, True)
-
-
-def tarot_main(message):
-    chat_id = message.chat.id
-    user_db_id = get_user_db_id(chat_id)
-    tarot_delay = 1.5
-
-    if platform == 'win32':
-        tarot_path = 'Tarot'
-        char = '\\'
-    else:
-        tarot_path = path.join(getcwd(), 'Tarot')
-        char = '/'
-
-    sent_message = BOT.send_message(
-        message.chat.id,
-        '<b>Расклад Таро - это всего лишь инструмент для '
-        'ознакомления и развлечения. '
-        'Расклад карт Таро не является истиной и не должен '
-        'использоваться для принятия важных решений.</b>'
-        '\n'
-        f'\n<u>{ORG_NAME}</u> и его сотрудники не несут '
-        'ответственности за любые действия и их последствия, '
-        'которые повлекло использование данного расклада карт Таро.',
-        parse_mode='html'
-    )
-    sleep(tarot_delay)
-
-    record_message_id_to_db(user_db_id, sent_message.message_id)
-
-    cards = glob(f'{tarot_path}/*.jpg')
-    user_random_cards = []
-
-    while len(user_random_cards) < 3:
-        card = choice(cards)
-        card_num = int(card.split(char)[-1].split('.')[0])
-
-        if (card_num not in
-                [int(c.split(char)[-1].split('.')[0]) for c in
-                 user_random_cards]):
-
-            if (card_num % 2 == 1 and card_num + 1 not in
-                    [int(c.split(char)[-1].split('.')[0]) for c in
-                     user_random_cards]):
-                user_random_cards.append(card)
-
-            elif (card_num % 2 == 0 and card_num - 1 not in
-                  [int(c.split(char)[-1].split('.')[0]) for c in
-                   user_random_cards]):
-                user_random_cards.append(card)
-
-    captions = ['Прошлое', 'Настоящее', 'Будущее']
-
-    for card, caption in zip(user_random_cards, captions):
-        with open(card, 'rb') as photo:
-            with open(f'{card[:-4]}.txt', encoding='utf-8') as text:
-                description = text.read()
-
-            tarot_message = BOT.send_photo(
-                message.chat.id, photo,
-                caption=f'<b>{caption}</b>: {description}',
-                parse_mode='html'
-            )
-
-            record_message_id_to_db(user_db_id, tarot_message.message_id)
-
-            sleep(tarot_delay)
 
 
 @BOT.message_handler(content_types=['text', 'photo'])
